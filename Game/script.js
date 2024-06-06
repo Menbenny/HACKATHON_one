353f2032a639891dd22166b50aa1e5f08cfb1a6c
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-06 19:42:51 +0300

    Food list added

---
+++ Game/script.js
@@ -0,0 +1,40 @@
+// Food list 
+
+// English array 
+// dishes: 
+// Pizza
+// Sushi
+// Tacos
+// Spaghetti
+// Biryani
+// Burger
+// Fried Chicken
+// Falafel
+// Paella
+// Kimchi // 10
+
+// Fruits:
+// Apple
+// Banana
+// Orange
+// Strawberry
+// Mango
+// Pineapple
+// Grapes
+// Blueberry
+// Watermelon
+// Kiwi //10 
+
+// vegetables:
+// Carrot
+// Broccoli
+// Spinach
+// Cauliflower
+// Bell Pepper
+// Tomato
+// Zucchini
+// Eggplant
+// Kale
+// Cabbage //10
+
+// total 30
---
+++ Opening/script.js
@@ -0,0 +1,2 @@
+// Are you ready prompt 
+// if ready, start the game 
