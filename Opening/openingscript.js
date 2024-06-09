46b407afc88d57aedc885967219aa5515e50f786
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-09 17:54:33 +0300

    opening page background fix

--- Opening/openingscript.js
+++ Opening/openingscript.js
@@ -1,55 +1,21 @@
-// Are you ready prompt 
+// Are you ready prompt (Bootstrap -- MODAL)
 // if ready, start the game 
 
-// startGame = () => {
-//     const userName = prompt(`Enter player name: `);
-//     if (userName) {
-//         localStorage.setItem('userName', userName);
-//         window.location.href = '/HACKATHON_one/Game/gameindex.html'
-//     }
-// }
-// startGame()
-
-
-
-document.getElementById('startButton').addEventListener('click', function() {
-    document.getElementById('placeholder').style.display = 'block';
-  });
   
   document.getElementById('submitButton').addEventListener('click', function() {
     const playerName = document.getElementById('playerName').value;
     if (playerName) {
         localStorage.setItem('userName', playerName);
-        window.location.href = '/HACKATHON_one/Game/gameindex.html';
+        window.location.href = '/Game/gameindex.html';
     }
   });
 
-//   Modal
 
-// document.getElementById('submitButton').addEventListener('click', function() {
-//     const playerName = document.getElementById('playerName').value;
-//     if (playerName) {
-//         localStorage.setItem('userName', playerName);
-//         window.location.href = '/HACKATHON_one/Game/gameindex.html';
-//     }
-// });
-
-
-// const myModal = document.getElementById('myModal')
-// const myInput = document.getElementById('myInput')
-
-// myModal.addEventListener('shown.bs.modal', () => {
-//   myInput.focus()
-// })
-
-document.getElementById('startButton').addEventListener('click', function() {
-    // The modal will be toggled by Bootstrap's data-bs-toggle attribute
-});
 
 document.getElementById('submitButton').addEventListener('click', function() {
     const playerName = document.getElementById('playerName').value;
     if (playerName) {
         localStorage.setItem('userName', playerName);
-        window.location.href = '/HACKATHON_one/Game/gameindex.html';
+        window.location.href = '/Game/gameindex.html';
     }
 });
